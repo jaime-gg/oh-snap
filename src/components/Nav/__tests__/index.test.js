5b1eb944b4a1f1582eeedc4aff2a47af2d4343cb
jester <jaimebuddy98@gmail.com>
2022-04-01 18:27:48 -0500

    ran more tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,13 +3,23 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     // First Test
     it('renders', () => {
-        render(<Nav />);
-    });
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
+    })
 
     // Second Test
     it('matches snapshot DOM node structure', () => {
@@ -26,7 +36,7 @@ describe('emoji is visible', () => {
 
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     });
-})
+}) 
 
 
 describe('links are visible', () => {
